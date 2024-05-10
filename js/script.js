45cc4e46b9ea743348f0d75a78329b1cbeb01d01
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-05-10 22:09:15 +0000

    add webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
+
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * this function find type of triangle
+ */
+
+function calculteQuotientOfTwoNumber() {
+  // input
+  const dividendNumber = parseInt(document.getElementById("dividend-number").value)
+  const divisorNumber = parseInt(document.getElementById("divisor-number").value)
+  let remainderOfTwoNumbers = 0
+  let counter = 0
+
+  // process
+  remainderOfTwoNumbers = dividendNumber - divisorNumber
+  counter++
+
+  while (true) {
+    if (remainderOfTwoNumbers < divisorNumber) {
+      break
+    } else {
+      remainderOfTwoNumbers = remainderOfTwoNumbers - divisorNumber
+      counter++
+      
+    }
+  }
+  // output
+  document.getElementById("result") = "The answer of " + dividendNumber + "/" + divisorNumber + " is: " + counter + " R " + remainderOfTwoNumbers
+}
